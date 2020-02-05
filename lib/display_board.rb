fb2f959bfc2388c797bfc42f4dc73ff68cc66e7d
Brian Vehige <brianvehige@gmail.com>
2020-02-05 04:28:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def vertical
+def display_board
   puts "   |   |   "
+  
 end
-
-vertical
+display_board
 
 def horizontal
   puts "-----------"
